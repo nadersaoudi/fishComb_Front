4851f79427d3240ae2e636a4f264f2218c5cc61b
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-01-25 00:23:23 +0100

    upload image event

--- src/Actions/events.js
+++ src/Actions/events.js
@@ -4,31 +4,17 @@ import Cookies from 'js-cookie';
 import { toast } from 'react-toastify';
 toast.configure();
 //Add event
-export const addEvent = ({ name,
-    description,
-    category_id,
-    location,
-    date,
-    cover,
-    video_link,
-    status }) => async dispatch => {
+export const addEvent = file => async dispatch => {
 
         const config = {
             headers: {
                 Authorization: 'Bearer ' + Cookies.get('user'),
-                'content-Type': 'application/json'
+                'Content-Type': 'multipart/form-data'
             }
         }
         try {
 
-            const res = await axios.post(`/api/events`, { name,
-                description,
-                category_id,
-                location,
-                date,
-                cover,
-                video_link,
-                status }, config)
+            const res = await axios.post(`/api/events`,file, config)
            
             dispatch({
                 type: ADD_EVENT,
