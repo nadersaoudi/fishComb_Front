39f5f5fe063202f1b6cc1830a109f6b9ac2cef05
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-02-14 20:47:19 +0100

    fix-spinner

--- src/components/pages/Spinner/Spinner.js
+++ src/components/pages/Spinner/Spinner.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Row, Col,Image } from 'react-bootstrap';
+import {Row, Col, Image} from 'react-bootstrap';
 import './Spinner.css'
-const Spinner =()=>{
-return <Row>
-    <Col sm={6}></Col>
-   <Col sm={2}> <Image src='logofish.png'  alt='fishcomb spinner' className='rotate' /></Col></Row>
- 
+const Spinner = () => {
+    return <Row className='pt-5 pb-5'>
+        <Row className='pt-5 pb-5'>
+        <Col sm={5}></Col>
+        <Col sm={2}
+            className='pt-5'>
+            <Image src='logofish.png' alt='fishcomb spinner' className='rotate'/>
+        </Col></Row>
+    </Row>
+
 }
 export default Spinner;
