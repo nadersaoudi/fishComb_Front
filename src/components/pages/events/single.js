215b94dc378b814624af33124e56328758ac8993
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2021-02-14 16:59:43 +0100

    fix index logo

--- src/components/pages/events/single.js
+++ src/components/pages/events/single.js
@@ -11,7 +11,7 @@ const Single =({  event })=> {
               
         <div className='image_holder grid '>
         <NavLink to={`/dashboard/singleevent/${event.id}`} > 
-        <img src={event && event.cover} width="100%" height="100" alt='event'  style={{borderRadius:'5px'}} /></NavLink>
+        <img src={event && event.cover} width="100%" height="100" alt='event'  style={{borderRadius:'5px'}} className='product' /></NavLink>
             <div className='description'>
                 <span><b>{event && event.name.charAt(0).toUpperCase() + event.name.slice(1) }</b></span> <br /> 
                 <Moment
